092ae137c951cfe47ff70e993a5b74a81064978f
VictoriaNaenfeldtEklund <victoria.naenfeldt.eklund@iths.se>
2024-11-04 15:59:11 +0100

    Add ObservableList to Model and bind it to buttontexts in view.

--- src/main/java/com/example/laboration3/controller/controllerTicTacToe.java
+++ src/main/java/com/example/laboration3/controller/controllerTicTacToe.java
@@ -1,6 +1,7 @@
 package com.example.laboration3.controller;
 
 import com.example.laboration3.model.Model;
+import com.example.laboration3.model.SquareIndex;
 import javafx.scene.input.MouseEvent;
 
 public class controllerTicTacToe {
@@ -12,29 +13,38 @@ public class controllerTicTacToe {
     }
 
     public void square0Clicked(MouseEvent mouseEvent) {
+        model.setSquareSymbol(SquareIndex.ZERO);
     }
 
     public void square1Clicked(MouseEvent mouseEvent) {
+        model.setSquareSymbol(SquareIndex.ONE);
     }
 
     public void square2Clicked(MouseEvent mouseEvent) {
+        model.setSquareSymbol(SquareIndex.TWO);
     }
 
     public void square3Clicked(MouseEvent mouseEvent) {
+        model.setSquareSymbol(SquareIndex.THREE);
     }
 
     public void square4Clicked(MouseEvent mouseEvent) {
+        model.setSquareSymbol(SquareIndex.FOUR);
     }
 
     public void square5Clicked(MouseEvent mouseEvent) {
+        model.setSquareSymbol(SquareIndex.FIVE);
     }
 
     public void square6Clicked(MouseEvent mouseEvent) {
+        model.setSquareSymbol(SquareIndex.SIX);
     }
 
     public void square7Clicked(MouseEvent mouseEvent) {
+        model.setSquareSymbol(SquareIndex.SEVEN);
     }
 
     public void square8Clicked(MouseEvent mouseEvent) {
+        model.setSquareSymbol(SquareIndex.EIGHT);
     }
 }
--- src/main/java/com/example/laboration3/model/Model.java
+++ src/main/java/com/example/laboration3/model/Model.java
@@ -1,4 +1,24 @@
 package com.example.laboration3.model;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import java.util.stream.IntStream;
+
+import static com.example.laboration3.model.GameState.*;
+
 public class Model {
+
+    private ObservableList<String> squares = FXCollections.observableArrayList();
+
+    public Model(){
+        IntStream.range(0, 9).forEach(_ -> squares.add(""));
+    }
+
+    public ObservableList<String> getSquares() {
+        return squares;
+    }
+
+    public void setSquareSymbol(SquareIndex squareIndex){
+        squares.set(squareIndex.ordinal(), "S");
+    }
 }
