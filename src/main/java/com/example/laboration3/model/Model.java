1a0f86c9579436b6352f31ec58116920e1659e8e
VictoriaNaenfeldtEklund <victoria.naenfeldt.eklund@iths.se>
2024-11-04 17:09:54 +0100

    Add gameMessage to Model and bind it to view.

--- src/main/java/com/example/laboration3/model/Model.java
+++ src/main/java/com/example/laboration3/model/Model.java
@@ -2,15 +2,19 @@ package com.example.laboration3.model;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+
+import java.util.Random;
 import java.util.stream.IntStream;
 
 import static com.example.laboration3.model.GameState.*;
 
 public class Model {
 
+    Random random = new Random();
     private GameState currentGameState = GAME_OVER;
     private GameState lastGameState = GAME_OVER;
     private String gamesPlayed = "0";
+    private String gameMessage = "";
 
     private Player player1 = new Player("Player 1", "X", "0");
     private Player player2 = new Player("Player 2", "O", "0");
@@ -31,12 +35,18 @@ public class Model {
     }
 
     private void resetGame(GameState newGameState) {
-        resetSquaresToEmpty();
 
         if(currentGameState != newGameState){
             resetPlayerPointsToZero();
+            gamesPlayed = "0";
         }
 
+        resetSquaresToEmpty();
+        setCurrentPlayerToRandomStartPlayer();
+    }
+
+    private void setCurrentPlayerToRandomStartPlayer() {
+       currentPlayer = random.nextInt(2) == 0 ? player1 : player2;
     }
 
     private void resetSquaresToEmpty() {
@@ -48,14 +58,16 @@ public class Model {
         player2.setScore("0");
     }
 
-    public ObservableList<String> getSquares() {
-        return squares;
-    }
-
     public void setSquareSymbol(SquareIndex squareIndex){
         if(squares.get(squareIndex.ordinal()).isEmpty()){
             squares.set(squareIndex.ordinal(), currentPlayer.getSymbol());
         }
+    }
+
+    // GET METHODS
+
+    public ObservableList<String> getSquares() {
+        return squares;
     }
 
     public Player getPlayer1() {
@@ -68,5 +80,9 @@ public class Model {
 
     public String getGamesPlayed() {
         return gamesPlayed;
+    }
+
+    public String getGameMessage() {
+        return gameMessage;
     }
 }
