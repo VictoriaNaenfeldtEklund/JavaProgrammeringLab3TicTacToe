cf44867d8f3890268ac25911b6594b19b7b70acf
VictoriaNaenfeldtEklund <victoria.naenfeldt.eklund@iths.se>
2024-11-04 15:02:52 +0100

    New view design. Clean up in Controller and Model.

--- src/main/java/com/example/laboration3/controller/controllerTicTacToe.java
+++ src/main/java/com/example/laboration3/controller/controllerTicTacToe.java
@@ -1,9 +1,6 @@
 package com.example.laboration3.controller;
 
 import com.example.laboration3.model.Model;
-import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.input.MouseEvent;
 
 public class controllerTicTacToe {
 
@@ -11,21 +8,5 @@ public class controllerTicTacToe {
 
     public Model getModel() {
         return model;
-    }
-
-    @FXML
-    public void squareButtonClicked(MouseEvent mouseEvent) {
-        var square = (Button) mouseEvent.getSource();
-        var squareSymbol = square.getText();
-
-        if (squareSymbol.isEmpty()){
-            model.play(square.getId());
-        }
-    }
-
-    public void startButtonClicked(MouseEvent mouseEvent) {
-        var button = (Button) mouseEvent.getSource();
-        var buttonId = button.getId();
-        model.startGame(buttonId);
     }
 }
---
+++ src/main/java/com/example/laboration3/model/SquareIndex.java
@@ -0,0 +1,5 @@
+package com.example.laboration3.model;
+
+public enum SquareIndex {
+    ZERO, ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT
+}
