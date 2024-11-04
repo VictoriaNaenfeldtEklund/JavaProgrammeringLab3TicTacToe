417f469621f85479bf7bb6b779229c6e0f5193dd
VictoriaNaenfeldtEklund <victoria.naenfeldt.eklund@iths.se>
2024-11-04 15:05:02 +0100

    Add mouseEvent to Square buttons.

--- src/main/java/com/example/laboration3/controller/controllerTicTacToe.java
+++ src/main/java/com/example/laboration3/controller/controllerTicTacToe.java
@@ -1,6 +1,7 @@
 package com.example.laboration3.controller;
 
 import com.example.laboration3.model.Model;
+import javafx.scene.input.MouseEvent;
 
 public class controllerTicTacToe {
 
@@ -8,5 +9,32 @@ public class controllerTicTacToe {
 
     public Model getModel() {
         return model;
+    }
+
+    public void square0Clicked(MouseEvent mouseEvent) {
+    }
+
+    public void square1Clicked(MouseEvent mouseEvent) {
+    }
+
+    public void square2Clicked(MouseEvent mouseEvent) {
+    }
+
+    public void square3Clicked(MouseEvent mouseEvent) {
+    }
+
+    public void square4Clicked(MouseEvent mouseEvent) {
+    }
+
+    public void square5Clicked(MouseEvent mouseEvent) {
+    }
+
+    public void square6Clicked(MouseEvent mouseEvent) {
+    }
+
+    public void square7Clicked(MouseEvent mouseEvent) {
+    }
+
+    public void square8Clicked(MouseEvent mouseEvent) {
     }
 }
